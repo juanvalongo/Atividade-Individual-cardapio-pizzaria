d4b5dd9338b03d47fa89cd909792123577b87c2d
JUAN SILVA VALONGO <juan.valongo5934264@edu.pe.senac.br>
2026-08-21 10:29:06 -0300

    inicia integração com API de receitas

--- scripts.js
+++ scripts.js
@@ -25,3 +25,36 @@ sobremesas.forEach(function(sobremesa) {
         }
     });
 }); 
+
+const url = "https://www.themealdb.com/api/json/v1/1/random.php"; //Isso simplesmente guarda o endereço da API em uma variável.
+
+const containerReceita = document.querySelector("#receita-api");
+/* O que document.querySelector() está fazendo?
+
+Esta linha:
+
+document.querySelector("#receita-api");
+
+está procurando no HTML:
+
+<div id="receita-api">
+
+O # significa que estamos procurando um ID.
+
+Então:
+
+const containerReceita = ...
+
+guarda esse elemento em uma variável. */
+
+fetch(url) //fetch() é uma função do JavaScript usada para fazer uma requisição HTTP. Estamos dizendo: "JavaScript, vá buscar os dados nesse endereço."
+    .then(response => response.json()) //A API responde em JSON. Esse comando transforma a resposta em um objeto JavaScript que podemos manipular.
+    .then(data => {
+        //console.log(data); //Agora data contém o resultado da API.
+        const receita = data.meals[0]; //Estamos dizendo: "Dentro dos dados recebidos, encontre meals e pegue o primeiro elemento." Então: data.meals[0] significa: "Pegue a primeira receita que está dentro de meals."
+        //console.log(receita.strMeal); //"Agora mostre somente essa receita."
+        containerReceita.innerHTML = receita.strMeal; //Agora, em vez de aparecer apenas no Console, o nome da receita deverá aparecer no seu site.
+    });
+
+    
+  
